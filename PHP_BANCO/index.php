7ad55bf90bc46cb92d3b66c3e91bc1014589d885
Nelson Modenez Neto <99834482+NelsonModenezNeto@users.noreply.github.com>
2023-09-19 11:36:52 -0300

    ee
    ee

--- PHP_BANCO/index.php
+++ PHP_BANCO/index.php
@@ -1,34 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>CRUD - Controle de alunos</title>
-
-    <style>
-        div {
-            border: 2px solid black;
-            width: 25%;
-            padding-left: 20%;
-            padding-bottom: 3%;
-            padding-top: 3%;
-            margin-left: 25%;
-        }
-        h2 {
-            margin-left: 40%;
-        }
-    </style>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
+
 <body>
 
-  <h2>Controle de Alunos</h2>
-  <div>
-      <form>
-          <input type="button" value="Cadastrar"
-                 onclick="window.open('cadastra.php', '_top');"><br><br>
-          <input type="button" value="Consultar"
-                 onclick="window.open('consulta.php', '_top');">
-      </form>
-  </div>
+    <div class="container-fluid">
+        <div class="row align-items-center" style="height: 100vh; margin-left: 60px;">
+            <div class="col-3">
+
+                <div class="d-grid gap-2 ">
+                    <h1 class="text-center">GERENCIAMENTO</h1>
+                    <button type="button" class="btn" style="background-color: rgb(255, 94, 30); color: white">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                            class="bi bi-plus-square-fill" viewBox="0 0 16 16">
+                            <path
+                                d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z">
+                            </path>
+                        </svg>
+                        Button
+                    </button>
+                    <button type="button" class="btn" style="background-color: rgb(255, 94, 30); color: white">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                            class="bi bi-plus-square-fill" viewBox="0 0 16 16">
+                            <path
+                                d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z">
+                            </path>
+                        </svg>
+                        Button
+                    </button>
+                    <button type="button" class="btn" style="background-color: rgb(255, 94, 30); color: white">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                            class="bi bi-plus-square-fill" viewBox="0 0 16 16">
+                            <path
+                                d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z">
+                            </path>
+                        </svg>
+                        Button
+                    </button>
+                    <button type="button" class="btn" style="background-color: rgb(255, 94, 30); color: white">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                            class="bi bi-plus-square-fill" viewBox="0 0 16 16">
+                            <path
+                                d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z">
+                            </path>
+                        </svg>
+                        Button
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </body>
+
 </html>
