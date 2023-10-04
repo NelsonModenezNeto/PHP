96e261c70f2e2f2e7f909550f4dc49bc65b357d1
julyav <99834482+NelsonModenezNeto@users.noreply.github.com>
2023-10-04 10:38:04 -0300

    boleto
    boleto

---
+++ Boleto/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '70d4ec5c29252b3afa9f8bb6c2f8baa2740b21ae',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '70d4ec5c29252b3afa9f8bb6c2f8baa2740b21ae',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'kriansa/openboleto' => array(
+            'pretty_version' => 'v1.0',
+            'version' => '1.0.0.0',
+            'reference' => 'ae70c2502b12cfbdad28377cddd8014031de76ce',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../kriansa/openboleto',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
